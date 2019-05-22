4eec7aaedb737a570b12b72070e5a2fa81d367f1
James Burke <james.burke1203@gmail.com>
2019-05-22 21:38:54 +0200

    add functional tests

---
+++ functional_test.py
@@ -0,0 +1,42 @@
+#!env/bin/python3
+import unittest
+from boddle import boddle
+import logging
+
+logger = logging.getLogger(__name__)
+
+from home import view as home
+
+SERVICES = (home,)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class FunctionalTestCase(unittest.TestCase):
+
+    def setUp(self):
+        Session = sessionmaker()
+        self.engine = create_engine("sqlite:///:memory:", echo=False)
+        Session.configure(bind=self.engine)
+        Base.metadata.create_all(self.engine)
+        self.db = Session()
+
+        for service in SERVICES:
+            service.app.TEMPLATE_PATH.append('./{service}/templates/'.format(service=service.__name__.split('.')[0]))
+
+    def tearDown(self):
+        Base.metadata.drop_all(self.engine)
+
+    def test_get_home(self):
+
+        with boddle(method='GET'):
+            response = home.index(db=self.db)
+            assert 'Welcome to Bottle-rack!' in response
+
+
+if __name__ == '__main__':
+    unittest.main()
