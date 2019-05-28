afabfd7fd8e77b22a98730c4af581f24931640a3
James Burke <james.burke1203@gmail.com>
2019-05-28 21:28:30 +0200

    get services from services directory

--- functional_test.py
+++ functional_test.py
@@ -1,16 +1,14 @@
 #!env/bin/python3
+import os
 import unittest
 from boddle import boddle
 import logging
 
+# temp until mold is working
 import sys
 sys.path.append('../bottle-mold/')
 
 logger = logging.getLogger(__name__)
-
-from home import view as home
-
-SERVICES = (home,)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -22,23 +20,33 @@ Base = declarative_base()
 class FunctionalTestCase(unittest.TestCase):
 
     def setUp(self):
+        # create database session
         Session = sessionmaker()
         self.engine = create_engine("sqlite:///:memory:", echo=False)
         Session.configure(bind=self.engine)
         Base.metadata.create_all(self.engine)
         self.db = Session()
 
-        for service in SERVICES:
-            service.app.TEMPLATE_PATH.append('./{service}/templates/'.format(service=service.__name__.split('.')[0]))
+        # import services
+        self.services = dict()
+        for service in list(filter(lambda f: f.is_dir(), list(os.scandir('./services')))):
+            mod = __import__('services.{service}.view'.format(service=service.name), fromlist=['app'])
+            self.services[service.name] = mod #getattr(mod, 'app')
+            self.services[service.name].app.TEMPLATE_PATH.append('./services/{service}/templates/'.format(service=service.name))
 
     def tearDown(self):
         Base.metadata.drop_all(self.engine)
 
-    def test_get_home(self):
-
+    def test_home_get(self):
         with boddle(method='GET'):
-            response = home.index(db=self.db)
+            response = self.services['home'].index(db=self.db)
             assert 'Welcome to Bottle-rack!' in response
+
+    def test_list_create_item(self):
+        with boddle(method='GET'):
+            response = self.services['list'].create(db=self.db)
+            assert 'Welcome to Bottle-rack!' in response
+
 
 
 if __name__ == '__main__':
